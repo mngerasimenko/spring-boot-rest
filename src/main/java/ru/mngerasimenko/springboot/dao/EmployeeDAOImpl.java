aec413860d13b866672d8a22b43cd44f4e6a443b
mngerasimenko <mngerasimenko@gmail.com>
2023-08-21 13:45:52 +0400

    only jpa

--- src/main/java/ru/mngerasimenko/springboot/dao/EmployeeDAOImpl.java
+++ src/main/java/ru/mngerasimenko/springboot/dao/EmployeeDAOImpl.java
@@ -1,12 +1,11 @@
 package ru.mngerasimenko.springboot.dao;
 
-import org.hibernate.Session;
-import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.mngerasimenko.springboot.entity.Employee;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
@@ -21,33 +20,42 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
 	@Override
 	public List<Employee> getAll() {
-		Session session = entityManager.unwrap(Session.class);
-		Query<Employee> query = session.createQuery("from Employee", Employee.class);
+//		Session session = entityManager.unwrap(Session.class);
+//		Query<Employee> query = session.createQuery("from Employee", Employee.class);
+
+		Query query = entityManager.createQuery("from Employee");
 
 		return query.getResultList();
 	}
 
 	@Override
 	public void save(Employee employee) {
-		Session session = entityManager.unwrap(Session.class);
+//		Session session = entityManager.unwrap(Session.class);
+//		session.saveOrUpdate(employee);
 
-		session.saveOrUpdate(employee);
+		Employee newEmployee = entityManager.merge(employee);
+		employee.setId(newEmployee.getId());
 	}
 
 	@Override
 	public Employee getById(int id) {
-		Session session = entityManager.unwrap(Session.class);
+//		Session session = entityManager.unwrap(Session.class);
+//		return session.get(Employee.class, id);
 
-		return session.get(Employee.class, id);
+		return entityManager.find(Employee.class, id);
 	}
 
 	@Override
 	public void delete(int id) {
-		Session session = entityManager.unwrap(Session.class);
-		@SuppressWarnings("unchecked")
-		Query<Employee> query = session.createQuery("delete from Employee where id =: employeeId");
-		query.setParameter("employeeId", id);
+//		Session session = entityManager.unwrap(Session.class);
+//		@SuppressWarnings("unchecked")
+//		Query<Employee> query = session.createQuery("delete from Employee where id =: employeeId");
+//		query.setParameter("employeeId", id);
+//
+//		query.executeUpdate();
 
+		Query query = entityManager.createQuery("delete from Employee where id =: employeeId");
+		query.setParameter("employeeId", id);
 		query.executeUpdate();
 	}
 }
